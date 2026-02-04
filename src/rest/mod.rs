ab7c8c3b9ea1cffa419deffcebec0d782548d667
Josue Figueroa <poor.rican@pm.me>
2026-02-04 16:06:21 -0700

    feat(rest): Export CursorPager from rest module

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,5 +1,5 @@
 mod client;
 pub mod types;
 
-pub use client::{KalshiRestClient, RateLimitConfig, RateLimitTier};
+pub use client::{CursorPager, KalshiRestClient, RateLimitConfig, RateLimitTier};
 pub use types::*;
